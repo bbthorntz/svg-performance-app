a956180b79d43e275dbf6bc0af82002977768041
luker <luker@indeed.com>
2022-08-22 16:16:56 +0900

    Add cname

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/svg-performance-app/',
+    base: '/',
     plugins: [react()],
     resolve: {
         alias: {
