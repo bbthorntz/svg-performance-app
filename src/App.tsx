1705ee0f8fc904939acee1defd2756bfe4a9ffa0
luker <luker@indeed.com>
2022-08-23 12:28:00 +0900

    Add 'img' render method

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,78 @@
-import { useState, useRef, useEffect } from 'react'
-import ReactHtmlParser from 'html-react-parser'
+import { useState, useRef, useEffect } from 'react';
+import ReactHtmlParser from 'html-react-parser';
 // @ts-ignore
-import getSvgoInstance from 'svgo-browser/lib/get-svgo-instance'
-import exampleSvg from './example-svg'
-import './index.css'
+import getSvgoInstance from 'svgo-browser/lib/get-svgo-instance';
+import exampleSvg from './assets/example-svg';
 
 // everything included in default configuration
 const svgo = getSvgoInstance({
     removeDimensions: false,
-})
+});
 
 export default function App() {
-    const svgStrRef = useRef<HTMLInputElement>(null)
-    const svgCountRef = useRef<HTMLInputElement>(null)
-    const svgRenderMethodRef = useRef<HTMLSelectElement>(null)
-    const svgSvgoRef = useRef<HTMLSelectElement>(null)
-    const [svgs, setSvgs] = useState<React.ReactNode>()
-    const [lastRenderTime, setLastRenderTime] = useState<number>(0)
+    const svgStrRef = useRef<HTMLInputElement>(null);
+    const svgCountRef = useRef<HTMLInputElement>(null);
+    const svgRenderMethodRef = useRef<HTMLSelectElement>(null);
+    const svgSvgoRef = useRef<HTMLSelectElement>(null);
+    const [svgs, setSvgs] = useState<React.ReactNode>();
+    const [lastRenderTime, setLastRenderTime] = useState<number>(0);
 
-    const startTime = performance.now()
+    const startTime = performance.now();
 
     useEffect(() => {
-        if (!svgs) return
-        const endTime = performance.now()
-        setLastRenderTime(endTime - startTime)
+        if (!svgs) return;
+        const endTime = performance.now();
+        setLastRenderTime(endTime - startTime);
         // console.log("Time:", endTime - startTime);
-    }, [svgs])
+    }, [svgs]);
 
     const handleSubmit = async (event: React.SyntheticEvent) => {
-        event.preventDefault()
-        const svgStrOriginal = svgStrRef.current!.value
-        const svgCount = parseInt(svgCountRef.current!.value) || 1
-        const svgRenderMethod = svgRenderMethodRef.current!.value
-        const svgSvgo = svgSvgoRef.current!.value
+        event.preventDefault();
+        const svgStrOriginal = svgStrRef.current!.value;
+        const svgCount = parseInt(svgCountRef.current!.value) || 1;
+        const svgRenderMethod = svgRenderMethodRef.current!.value;
+        const svgSvgo = svgSvgoRef.current!.value;
 
         const svgStr =
             svgSvgo === 'optimise'
                 ? ((await svgo
                       .optimize(svgStrOriginal)
                       .then(({ data }: { data: string }) => data)) as string)
-                : svgStrOriginal
+                : svgStrOriginal;
 
-        let nextDivs = []
-        const element = ReactHtmlParser(svgStr)
+        let nextDivs = [];
+        const element = ReactHtmlParser(svgStr);
+        const encodedSvg = encodeURIComponent(svgStr);
 
         for (let i = 0; i < svgCount; i++) {
-            if (svgRenderMethod === 'dangerous') {
-                nextDivs.push(
-                    <span
-                        key={`${Date.now()}${i}`}
-                        dangerouslySetInnerHTML={{ __html: svgStr }}
-                    />
-                )
-            } else {
-                nextDivs.push(<span key={`${Date.now()}${i}`}>{element}</span>)
+            switch (svgRenderMethod) {
+                case 'dangerous':
+                    nextDivs.push(
+                        <span
+                            key={`${Date.now()}${i}`}
+                            dangerouslySetInnerHTML={{ __html: svgStr }}
+                        />
+                    );
+                    break;
+
+                case 'jsx':
+                    nextDivs.push(
+                        <span key={`${Date.now()}${i}`}>{element}</span>
+                    );
+                    break;
+
+                case 'img':
+                    nextDivs.push(
+                        <img
+                            key={`${Date.now()}${i}`}
+                            src={`data:image/svg+xml;utf8,${encodedSvg}`}
+                        />
+                    );
+                    break;
             }
         }
-        setSvgs(nextDivs)
-    }
+        setSvgs(nextDivs);
+    };
 
     return (
         <div className="h-full flex flex-col divide-y">
@@ -109,6 +124,7 @@ export default function App() {
                             dangerouslySetInnerHTML
                         </option>
                         <option value="jsx">JSX</option>
+                        <option value="img">Img</option>
                     </select>
                 </label>
                 <button
@@ -125,5 +141,5 @@ export default function App() {
                 </div>
             </div>
         </div>
-    )
+    );
 }
